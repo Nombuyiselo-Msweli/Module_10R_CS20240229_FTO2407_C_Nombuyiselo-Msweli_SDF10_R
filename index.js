fdf4ff75b9a6ebc53debd26ec1772dae0067c44b
Nombuyiselo Msweli <mswelinombuyiselo@gmail.com>
2024-08-01 16:43:05 +0200

    refactored by adding 2 functions to lay out clearing text box and adding item to the list

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const appSettings = {
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const shoppingListInDB = ref(database, "shoppingList")
+const shoppingListInDB = ref(database, "shoppingList")    //creates a "folder" on our database to which our items will be added
 
 //==== APP SECTION ====
 //input field functionality --> getting typed input, pushing it to the database and displaying it in an element of it's own
 let inputFieldEl = document.getElementById("input-field")
-let shoppingListEl = document.getElementById("shopping-list")
+let shoppingListEl = document.getElementById("shopping-list") // BUG: had to swap the position of variables. Error was that shoppingListEL was not defined at button event.
 let addButtonEl = document.getElementById("add-button")
 
 
@@ -23,15 +23,23 @@ addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     
     push(shoppingListInDB, inputValue)
+
+    clearInputFieldEl()                  //clears text box after input has been pushed
     
-    shoppingListEl.innerHTML += `<li>${inputValue}</li>`
+    updateShoppingListElWithItem(inputValue) //pushes new list item from text input to the list of items in the shopping cart
+
+    
 })
 
-// addButtonEl.addEventListener("click", function() {
-//     let inputValue = inputFieldEl.value
-    
-//     push(shoppingListInDB, inputValue)
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
 
-//     shoppingListEl.innerHTML += `<li>${inputValue}</li>`
-// })
+function  updateShoppingListElWithItem(itemValue) {
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+}
+
+
+
+
 
