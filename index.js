4122e4272dda8c4f9639ade46601cd8d57ef0e33
Nombuyiselo Msweli <mswelinombuyiselo@gmail.com>
2024-08-01 16:23:57 +0200

    updated project with list and added functionality to add item with button click + corrected DOCTYPE error

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//=== DATABASE CONNECTION SECTION ===
 //creating communication between the app and the database by firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref , push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
@@ -10,16 +11,27 @@ const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shoppingListInDB = ref(database, "shoppingList")
 
+//==== APP SECTION ====
+//input field functionality --> getting typed input, pushing it to the database and displaying it in an element of it's own
+let inputFieldEl = document.getElementById("input-field")
+let shoppingListEl = document.getElementById("shopping-list")
+let addButtonEl = document.getElementById("add-button")
 
-//input field functionality --> getting typed input and siplaying it in an element of it's own
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
+
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     
     push(shoppingListInDB, inputValue)
-
-    console.log(inputValue)
+    
+    shoppingListEl.innerHTML += `<li>${inputValue}</li>`
 })
+
+// addButtonEl.addEventListener("click", function() {
+//     let inputValue = inputFieldEl.value
+    
+//     push(shoppingListInDB, inputValue)
+
+//     shoppingListEl.innerHTML += `<li>${inputValue}</li>`
+// })
 
