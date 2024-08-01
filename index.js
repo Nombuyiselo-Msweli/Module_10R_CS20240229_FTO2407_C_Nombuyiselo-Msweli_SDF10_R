81939aac5c30b04082766debbb7ad716a9b1b5fb
Nombuyiselo Msweli <mswelinombuyiselo@gmail.com>
2024-08-01 15:35:14 +0200

    updated project with ability to communicate and push items to the firebase database

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
+//creating communication between the app and the database by firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref , push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
+const appSettings = {
+    databaseURL: "https://food-cart-15a72-default-rtdb.asia-southeast1.firebasedatabase.app/"    //link to firebase real time database
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+
+//input field functionality --> getting typed input and siplaying it in an element of it's own
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     
+    push(shoppingListInDB, inputValue)
+
     console.log(inputValue)
 })
 
